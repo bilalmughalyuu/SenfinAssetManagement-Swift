a4a35aad4fc50aa2999aa5b315672c354056f939
bilalmughalyuu <bilalmuhammad306@yahoo.com>
2025-05-28 17:10:05 +0400

    Add initial SwiftUIView with basic layout

---
+++ iosPractice/Views/Support/Support.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  iosPractice
+//
+//  Created by Muhammad Bilal on 28/05/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
